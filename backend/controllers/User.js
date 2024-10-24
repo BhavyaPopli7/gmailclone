66b39c84b66c9939d238c533b304639192b2a5d6
Bhavya Popli <121545442+BhavyaPopli7@users.noreply.github.com>
2024-10-24 21:33:13 +0530

    Update User.js

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -80,7 +80,7 @@ exports.login = async(req,res)=>{
         }
         const token = await jwt.sign(tokenData,process.env.SECRET_KEY,{expiresIn:'1d'});
         
-        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpOnly:true,smaeSite:'None', secure: true }).json({
+        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpOnly:true,sameSite:'None', secure: true }).json({
             message: `${user.fullname} logged in successfully.`,
             user,
             success:true
